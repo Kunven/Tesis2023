178d13b8c403d7c7327bd45feec5d1fc832422f9
Kunven <kunven@hotmail.es>
2023-06-20 04:57:05 -0500

    Added login screen

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Login } from './Login';
+import auth from '@react-native-firebase/auth';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+
+  if (!user) {
+    return (
+      <Login/>
+    );
+  }
+  return (
+    <View>
+      <Text>Aqui va la app</Text>
+      <Button
+        onPress={() => auth().signOut()}
+        title="Cerrar Sesion"
+        color="#841584"        
+      />
+    </View>
+  );  
+}
---
+++ Login.jsx
@@ -0,0 +1,135 @@
+import auth from '@react-native-firebase/auth';
+//import { useNavigation } from "@react-navigation/native";
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  ScrollView,  
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Pressable,
+  Keyboard,
+  Alert,
+} from "react-native";
+
+//import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
+export const Login = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const onPressLogin = () => {
+    // Do something about login operation
+    auth().signInWithEmailAndPassword(email,password)
+    .then(() =>{
+      Alert.alert('Login Correcto')
+    })
+    .catch(error =>{
+      if (error.code == 'auth/invalid-email' || error.code == 'auth/wrong-password') {
+        Alert.alert('Credenciales Incorrectas')
+      }else{
+        Alert.alert(error.code)
+      }
+    })
+  };
+  const onPressForgotPassword = () => {
+    // Do something about forgot password operation
+  };
+    const onPressSignUp = () => {
+    // Do something about signup operation
+  };
+
+  //const nav = useNavigation<NativeStackNavigationProp<any>>();
+
+  //const goToRegistration = () => {
+    //nav.push("Register");
+  //};
+
+  const goToMainFlow = async () => {
+    // Login Query
+  };
+
+  return (    
+    <Pressable style={styles.contentView} onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+      <Text style={styles.title}> LOGO</Text>      
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Email"
+          placeholderTextColor="#003f5c"
+          onChangeText={setEmail}/>
+      </View>
+      <View style={styles.inputView}>
+        <TextInput
+        style={styles.inputText}
+        secureTextEntry
+        placeholder="Password"
+        placeholderTextColor="#003f5c"
+        onChangeText={setPassword}/>
+      </View>
+      <TouchableOpacity
+        onPress = {onPressForgotPassword}>
+        <Text style={styles.forgotAndSignUpText}>Olvidaste tu Contraseña?</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress = {onPressLogin}
+        style={styles.loginBtn}>
+        <Text style={styles.loginText}>Inicio de Sesión </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress = {onPressSignUp}>
+        <Text style={styles.forgotAndSignUpText}>Registro</Text>
+      </TouchableOpacity>
+      </View>    
+    </Pressable>    
+  );
+};
+
+const styles = StyleSheet.create({
+  contentView: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#ccffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title:{
+    //fontWeight: "bold",
+    fontSize:50,
+    color:"black",
+    marginBottom: 0,
+  },  
+  inputView:{
+    width:"80%",
+    backgroundColor:"#ffffcc",
+    borderRadius:0,
+    height:50,
+    marginBottom:20,
+    justifyContent:"center",
+    padding:20
+  },
+  inputText:{
+    height:50,
+    color:"black"
+  },
+  forgotAndSignUpText:{
+    color:"black",
+    fontSize:11
+  },
+  loginBtn:{
+    width:"60%",
+    backgroundColor:"#ffcc66",
+    borderRadius:25,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:40,
+    marginBottom:10
+  },
+});
